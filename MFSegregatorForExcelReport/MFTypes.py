1bc0ab19678411c029be40436965a16016445dcf
Hemanth G <hemanthreddyvutla@gmail.com>
2021-09-24 16:53:39 +0000

    MF Segregation added

--- MFSegregatorForExcelReport/MFTypes.py
+++ MFSegregatorForExcelReport/MFTypes.py
@@ -1,6 +1,19 @@
 EQUITY="EQUITY"
-
+DEBT="DEBT"
+ELSS="ELSS"
 
 mf_types = {
-  "TATAMutualFund_MF": EQUITY
+  "Tata Digital India Fund - Direct Plan": EQUITY,
+  "Axis Long Term Equity Fund - Direct Plan":ELSS,
+  "Aditya Birla Sun Life Tax Relief 96 - Direct Plan":ELSS,
+  "IDFC Tax Advantage (ELSS) Fund - Direct Plan":ELSS,
+  "L&T Tax Advantage Fund - Direct Plan":ELSS,
+  "Nippon India Small Cap Fund - Direct Plan":EQUITY,
+  "Kotak Bond Short Term Fund - Direct Plan":DEBT,
+  "Axis Liquid Fund - Direct Plan":DEBT,
+  "Franklin India Liquid Fund - Direct Plan":DEBT,
+  "SBI Magnum Constant Maturity Fund - Direct Plan":DEBT,
+  "Motilal Oswal Nifty Bank Index Fund - Direct Plan":EQUITY,
+  "Axis Focused 25 Fund - Direct Plan":EQUITY,
+  "Mirae Asset Great Consumer Fund - Direct Plan":EQUITY
 }
--- MFSegregatorForExcelReport/main.py
+++ MFSegregatorForExcelReport/main.py
@@ -1,11 +1,42 @@
 
 import json
-import MFTypes from './MFTypes.py'
+from MFTypes import mf_types,EQUITY,ELSS,DEBT
 
 f = open('MFSegregatorForExcelReport/input.json',)
 response = json.load(f)
 
 
-for trade in response['data']['portfolio']:
-  print(trade['order_execution_time'], trade['quantity'], trade['price'])
+fundTypes={
+  EQUITY:[],
+  ELSS:[],
+  DEBT:[]
+}
+
+for fund in response['data']['portfolio']:
+  # print(fund['scheme_name'])
+  # print(fund['net'])
+  temp_fund={
+    'scheme_name':fund['scheme_name'],
+    'net': fund['net']
+  }
+
+  if mf_types[fund['scheme_name']]==EQUITY:
+    fundTypes[EQUITY].append(temp_fund)
+  elif mf_types[fund['scheme_name']]==ELSS:
+    fundTypes[ELSS].append(temp_fund)
+  elif mf_types[fund['scheme_name']]==DEBT:
+    fundTypes[DEBT].append(temp_fund)
+  
+
+print(":: Funds Totals ::\n")
+for fundType in fundTypes.keys():
+  total=0
+  print(":: " + fundType + " ::\n")
+  for fund in fundTypes[fundType]:
+    print(fund['scheme_name'] + " - " + str(fund["net"]))
+    print()
+    total+=fund['net']
+    print()
+  print(total)
   print()
+  
